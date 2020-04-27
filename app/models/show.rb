ca9dcd62f83f8d98b72abd3cd6f9fbde25920c30
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-04-27 23:47:42 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
 
   def actors_list
     self.actors.each do |actor|
-      "#{self.name}"
+      "#{self.first_name} #{self.last_name}"
       #binding.pry
     end
   end
