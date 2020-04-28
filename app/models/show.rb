3d4c74964d720ca28090d9e3dc3cfb4123f9baaf
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-04-28 00:13:47 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,7 +6,7 @@ class Show < ActiveRecord::Base
 
   def actors_list
     self.actors.each do |actor|
-      "#{actor.full_name.split}"
+      "#{actor.full_name.split(', ')}"
       #binding.pry
     end
   end
