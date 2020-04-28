4d1623a5182ff72fdc5d5eba7087cc04fd85b3a0
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2020-04-28 00:03:23 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,8 +6,8 @@ class Show < ActiveRecord::Base
 
   def actors_list
     self.actors.each do |actor|
-      "(#{actor.full_name}).flatten"
-      #binding.pry
+      "#{actor.full_name}"
+      binding.pry
     end
   end
 
